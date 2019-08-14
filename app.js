f6d4726a695194d1bbd66ec3ca8837c43b7a51f3
Cadar <cadar@dcc.ufmg.br>
2019-08-13 23:43:04 -0300

    first commit

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get('/gen/:pass', (req, res) => {
     res.send(`Hello ${req.params.pass}`)
 });
 
+app.get('/', (req, res) => {
+    res.send(`Hello From /`)
+});
+
 const port = process.env.port  || 3000;
 
 app.listen(port, ()=> console.log(`Listening on port ${port}...`));
